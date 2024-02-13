3d9658530d4d5826166f21dbda247ffe7845e026
gvyganovsky <gvyganovsky@gmail.com>
2024-02-13 16:20:18 +0300

    Валидация

--- models/RegForm.php
+++ models/RegForm.php
@@ -12,10 +12,38 @@ class RegForm extends Users
     public function rules()
     {
         return [
-            [['login', 'password', 'FIO', 'phone', 'email'], 'required'],
-            [['admin'], 'integer'],
             [['login', 'password', 'FIO', 'email'], 'string', 'max' => 255],
-            [['phone'], 'string', 'max' => 15],
+            [['login', 'password', 'FIO', 'phone', 'email'], 'required', 'message' => 'Обязательное поле'],
+            [
+                ['login'],
+                'unique',
+            ],
+            [
+                ['password'],
+                'match',
+                'pattern' => '/^.{6,}$/',
+                'message' => 'Минимальная длина пароля - 6 символов'
+            ],     
+            [
+                ['FIO'],
+                'match',
+                'pattern' => '/^[А-Яа-яЁё\s]+$/u',
+                'message' => 'Допустимы только кириллические символы и пробелы.'
+            ],           
+            [
+                ['phone'],
+                'match',
+                'pattern' => '/^\+7\(\d{3}\)-\d{3}-\d{2}-\d{2}$/',
+                'message' => 'Пожалуйста, введите номер в формате +7(XXX)-XXX-XX-XX'
+            ],            
+            [
+                ['passwordRepeat'],
+                'compare',
+                'compareAttribute' => 'password',
+                'message' => 'Пароли не совпадают'
+            ],
+            [['email'], 'email'],
+            [['phone'], 'string', 'max' => 17],
             [['login'], 'unique'],
         ];
     }
